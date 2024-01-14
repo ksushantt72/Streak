c1ed4ad851a477927bff9957c60bc819f27ef8cd
ksushantt72 <ksushantt72@gmail.com>
2024-01-14 23:13:20 +0530

    Day2Streak

---
+++ Day2/Peak Index in Mountain Array.cpp
@@ -0,0 +1,31 @@
+//852.Peak Inddex in MOuntain Array
+//LEETCODE 
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n=arr.size();
+        int start=0;
+        int end=n-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]<arr[mid+1]){
+                //Line A pe Hoon aage jaana hai
+                start=mid+1;
+            }
+            else{
+                //Line B pe hoon ya khud peak element pe hooon
+                //peeche jaana hai
+                end=mid;
+            }
+        }
+        return start;
+        
+    }
+};
