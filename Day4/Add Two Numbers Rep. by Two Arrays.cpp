2eb33031534b4c3c22a4fe1c16ede4173a89e7bd
ksushantt72 <ksushantt72@gmail.com>
2024-01-16 23:27:59 +0530

    Day4Commit

---
+++ Day4/Add Two Numbers Rep. by Two Arrays.cpp
@@ -0,0 +1,53 @@
+//GFG PROBLEM
+//Add Two Numbers Rep. by Two Arrays
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits.h>
+using namespace std;
+
+class Solution{
+    public:
+    string calc_Sum(int *a,int n,int *b,int m){
+        string ans;
+        int i = n-1;
+        int j = m-1;
+        int carry=0;
+        
+        while(i>=0 && j>=0){
+            int x=a[i]+b[j]+carry;
+            int digit=x%10;
+            ans.push_back(digit+'0');
+            carry=x/10;
+            i--;
+            j--;
+            
+        }
+        
+        while(i>=0){
+            int x=a[i]+0+carry;
+            int digit=x%10;
+            ans.push_back(digit+'0');
+            carry=x/10;
+            i--;
+            
+        }
+        while(j>=0){
+            int x=0+b[j]+carry;
+            int digit = x%10;
+            ans.push_back(digit +'0');
+            carry = x/10;
+            j--;
+            
+        }
+        if(carry){
+            ans.push_back(carry + '0');
+        }
+        while(ans[ans.size()-1]=='0'){
+            ans.pop_back();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+
+    }
+};
